4a747b0c230398038647abd3a1eae971112a9782
Maciej Dems <maciej.dems@p.lodz.pl>
2021-10-16 16:00:51 +0200

    Reorganized exercise

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Quadratic Equation
 
-Please write a code that calculates the roots of a quadratic equation with the form
+Please write a code that calculates real roots of a quadratic equation of the form
 
 <p style="text-align: center;"><em>a</em> <em>x</em><sup>2</sup> + <em>b</em> <em>x</em> + <em>c</em> = 0</p>
 
-For the coefficients defined at the beginning of the program in the appropriate variables. For now, we assume that the coefficient *a* is not zero and we allow complex solutions (which means that the equation always has exactly two solutions and we do not need to verify it). Calculation result should be printed using the `print` function in the form of two numbers separated by a space.
+For the coefficients defined at the beginning of the program in the appropriate variables. Calculation result must be stored
+in a tuple named '`solutions`' and printed at the very end of the program.
 
 In the file `exercise1.py` there is a program template with the definition of example equation parameters. It needs to be completed.
+
+**Hint:** To compute a square root, you may raise a number to the power of 0.5 (like `9**0.5`).
--- run_test.py
+++ run_test.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # coding: utf8
-import sys
-from io import StringIO
-from unittest.mock import patch
-import unittest
 env = globals().copy()
 
+import sys
+import os
+import unittest
 
-def runfile(name, skip=0, **kwargs):
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def runfile(name, skip=16, **kwargs):
     filename = name + '.py'
     with open(filename, encoding='utf8') as file:
         source = file.read()
@@ -15,43 +16,37 @@ def runfile(name, skip=0, **kwargs):
         source = source[1:]
     source = "\n".join([""] * skip + source.splitlines()[skip:]) + "\n"
     code = compile(source, filename, 'exec')
-    exec(code, env, kwargs)
+    vars = kwargs.copy()
+    vars['solutions'] = ()
+    exec(code, env, vars)
+    return vars.get('solutions')
 
-
-def patchio():
-    return lambda meth: patch('sys.stdout', new_callable=StringIO)(meth)
 
 ########################################################################################################################
 
 
 class TestRoots(unittest.TestCase):
+    def assert_roots(self, solutions, *ref):
+        solutions = sorted(solutions)
+        self.assertEqual(len(solutions), len(ref), "Wrong number of results")
+        for x, r in zip(solutions, ref):
+            self.assertAlmostEqual(x, r)
 
-    def assert_roots(self, stdout, res1, res2):
-        out = stdout.getvalue()
-        if not out:
-            raise ValueError("Nothing printed")
-        x1, x2 = out.split()
-        x1 = complex(x1)
-        x2 = complex(x2)
-        if x2.real < x1.real or (x1.real == x2.real and x2.imag < x1.imag):
-            x1, x2 = x2, x1
-        self.assertAlmostEqual(x1, res1)
-        self.assertAlmostEqual(x2, res2)
+    def test_sample0(self):
+        solutions = runfile('exercise1', a=1, b=2, c=3)
+        self.assert_roots(solutions)
 
-    @patchio()
-    def test_default(self, stdout):
-        runfile('exercise1')
-        self.assert_roots(stdout, (-1-1.4142135623730951j), (-1+1.4142135623730951j))
+    def test_sample1(self):
+        solutions = runfile('exercise1', a=1, b=-2, c=1)
+        self.assert_roots(solutions, 1.)
 
-    @patchio()
-    def test_sample1(self, stdout):
-        runfile('exercise1', 15, a=1, b=-2, c=1)
-        self.assert_roots(stdout, 1., 1.)
+    def test_sample2(self):
+        solutions = runfile('exercise1', a=3, b=-2, c=-1)
+        self.assert_roots(solutions, -0.33333333333333, 1.)
 
-    @patchio()
-    def test_sample2(self, stdout):
-        runfile('exercise1', 15, a=3, b=-2, c=-1)
-        self.assert_roots(stdout, -0.33333333333333, 1.)
+    def test_sampleL(self):
+        solutions = runfile('exercise1', a=0, b=-2, c=1)
+        self.assert_roots(solutions, 0.5)
 
 
 if __name__ == '__main__':
