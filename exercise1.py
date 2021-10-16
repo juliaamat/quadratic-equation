89bcaf0bb68f0021c99040e17f6da819ec318e93
Maciej Dems <maciej.dems@p.lodz.pl>
2021-10-16 16:00:51 +0200

    Reorganized exercise

--- exercise1.py
+++ exercise1.py
@@ -4,13 +4,26 @@ The program should print two roots of the quadratic equation of the form
 
 a x² + b x + c = 0
 
-The roots shold be printed in one line, separated by space
+The roots must be stored in a tuple named 'solutions' and printed at the very end of the program.
 """
 
+# This is sample input data
+
 a = 1
-b = 2
-c = 3
+b = 3
+c = 2
+
+# This is a tuple for results. You should override it with actual results
+
+solutions = ()
 
 # DO NOT CHANGE ANYTHING ABOVE THIS LINE!!!
 
-# Put your code below...
+
+# Put your code here...
+
+
+
+
+# At the end, the results are printed to the screen
+print(*solutions)
